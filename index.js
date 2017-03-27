60dd1edbb600e8765705fe1fae1fc36758197d42
Jose Enrique Ruiz Navarro <joseenriqueruiznavarro@gmail.com>
2017-03-27 09:43:33 +0200

    added any endpoints

--- index.js
+++ index.js
@@ -12,6 +12,26 @@ router.post('/test/', function(req, res) {
    res.sendStatus(200);
 });
 
+router.post('/test/', function(req, res) {
+   console.log(req.body);
+   res.sendStatus(200);
+});
+
+router.post('/metrics/', function(req, res) {
+  //console.log("METRICS------------------------------");
+   //console.log(req.body);
+   res.sendStatus(200);
+});
+
+
+router.post('/state/', function(req, res) {
+  console.log("STATES------------------------------");
+   console.log(req.body);
+   res.sendStatus(200);
+});
+
+
+
 app.use(router);
 
 app.listen(3000, function() {
